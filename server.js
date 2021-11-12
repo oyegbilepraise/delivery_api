98b5e578d99c625dbcf2087db650d07e185ddeae
Praise <oluwadhammieh@gmail.com>
2021-11-12 12:24:25 +0100

    updated qr link

--- server.js
+++ server.js
@@ -9,18 +9,48 @@ app.get('/', (req, res) => {
     res.send('Welcome')
 })
 
+let arr = []
+
 let welcome = '- Welcome to Delivery Service' + '<br>' + 'Kindly Enter the Order ID'
 
-app.post('/verify_order_id', async (req, res) => {
+app.post('/get_delivery_order', async (req, res) => {
     try {
-        let {message} = req.body
+        let {message, order_id} = req.body
         if(message == 'delivery') res.status(200).json(welcome)
+
+        arr.push(order_id)
+        let response = await axios.post('https://sellbackend.creditclan.com/parent/index.php/globalrequest/get_delivery_order', {order_id})
         
-        else{
-            let order_id = message;
-            let response = await axios.post('https://sellbackend.creditclan.com/parent/index.php/globalrequest/get_delivery_order', {order_id})
-            if(response) res.status(200).send(response.data)
-        }
+        
+            if(response.data.status == true){
+                console.log(arr);
+                let a = response.data.order
+                    let phone = a.phone_no
+                    let total_amount = a.total_price.total_amount 
+                    let msg = {
+                        status: true,
+                        message: {
+                            phone, total_amount
+                        },
+                        response: 'Type' + '[1] To Confirm' + ' ' +'[2] To Edit' + ' ' + '[3] To Cancel' + '[m] main menu'
+                    }
+                res.json(msg)
+            }
+            if(message == '1'){
+                let order_id = arr[0];
+                let response = await axios.post('https://sellbackend.creditclan.com/parent/index.php/globalrequest/get_delivery_order', {order_id})
+                let scan_msg = response.data.scan_message
+                let scan_link = response.data.scan_link
+                let resp = {
+                    status: true,
+                    message: (`To confirm customer has recieved the order, ` + scan_msg + ' ' + 'click the link below to san the qr code'),
+                    scan_link
+                }
+                res.send(resp)
+            }
+            if(response.data.status == false && message != '1') {
+                res.send(response.data)
+            }
     } catch (error) {
         res.status(500).json(error)
     }
@@ -29,5 +59,5 @@ app.post('/verify_order_id', async (req, res) => {
 const PORT = process.env.PORT || 4100;
 
 app.listen(PORT, () => {
-    console.log('Server connected success');
+    console.log('Server connected success to' + PORT);
 })
